16f37eb66d6316bf385b5f5d9dda23bcff7cda84
Muhammad Ali khan <alykhan2212@gmail.com>
2021-06-20 20:40:04 +0500

    Update csr.js

--- src/pages/csr.js
+++ src/pages/csr.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 // import axios from 'axios';
-import AxiosStream from "axios-stream";
+import {AxiosStream} from "axios-stream";
 
 // import * as fs from 'fs';
 // import path from 'path';
