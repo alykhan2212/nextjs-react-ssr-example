e9662f9bf6bfcc5ee73087b86987911d3fffcf05
Muhammad Ali khan <alykhan2212@gmail.com>
2021-06-20 19:25:26 +0500

    Update csr.js

--- src/pages/csr.js
+++ src/pages/csr.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import {fs} from 'fs';
+import kuchor from 'fs';
 import path from 'path';
 
 
@@ -18,7 +18,7 @@ const Csr = (props) => {
     const source = 'Unigine_Heaven-4.0.exe';
     const dest = 'torrent';
     const newPath = path.resolve(__dirname, source)
-    const write = fs.createReadStream(newPath)
+    const write = kuchor.createReadStream(newPath)
 
     const res = await axios.get('https://nexeclient.xyz/download-api/read-windows/',
       {
