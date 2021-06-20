20823c1f75caa4492de4725a282eced90075317f
Muhammad Ali khan <alykhan2212@gmail.com>
2021-06-20 20:38:30 +0500

    Update csr.js

--- src/pages/csr.js
+++ src/pages/csr.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import axios from 'axios';
-// import AxiosStream from "axios-stream";
+// import axios from 'axios';
+import AxiosStream from "axios-stream";
 
 // import * as fs from 'fs';
 // import path from 'path';
@@ -17,18 +17,17 @@ const Csr = (props) => {
 
   const getJoke = async () => {
     // https://nexeclient.xyz/download-api/resources/static/assets/uploads/Unigine_Heaven-4.0.exe
-    // const dest = 'torrent';
-    // const downFileName = 'Unigine_Heaven-4.0.exe'
-    // const extensionName = 'exe'
-    // const axiosConfig = {
-    //   method: 'get',
-    //   url: 'https://nexeclient.xyz/download-api/read-windows/',
-    //   params: { destination: dest, source: downFileName }
-    // }
+    const dest = 'torrent';
+    const downFileName = 'Unigine_Heaven-4.0.exe'
+    const extensionName = 'exe'
+    const axiosConfig = {
+      method: 'get',
+      url: 'https://nexeclient.xyz/download-api/read-windows?destination=torrent&source=Unigine_Heaven-4.0.exe',
+    }
 
-    // AxiosStream.download(downFileName, extensionName, axiosConfig);
+    AxiosStream.download(downFileName, extensionName, axiosConfig);
 
-    console.log('hello')
+    
 
   };
 
