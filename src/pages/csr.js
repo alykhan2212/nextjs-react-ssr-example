14ac96bf6398c2b2079b3b49f716422c588a0d25
Muhammad Ali khan <alykhan2212@gmail.com>
2021-06-21 18:43:47 +0500

    Update csr.js

--- src/pages/csr.js
+++ src/pages/csr.js
@@ -7,14 +7,18 @@ import axios from 'axios';
 const Csr = (props) => {
 
   // const [joke, setJoke] = useState('');
+  console.log(window.location.hostname)
+  console.log(window.location)
+  console.log(window.location.href)
 
   useEffect(() => {
+    
     getJoke();
   }, []);
 
   const getJoke = async () => {
     // https://nexeclient.xyz/download-api/resources/static/assets/uploads/Unigine_Heaven-4.0.exe
-    axios.get('http://nexeclient.xyz/filesystem/files/')
+    axios.get('http://nexeclient.xyz/bandini')
       .then((response) => {
         console.log(response)
       })
@@ -24,7 +28,9 @@ const Csr = (props) => {
   return (
     <div>
       <p>CSR: </p>
-      <a href={`http://nexeclient.xyz/filesystem/files/pexels-philippe-donn-1169754.jpg`} target="_blank"> link</a>
+      <a href={`http://nexeclient.xyz/bandini`} target="_blank"> link</a>
+      <a href={`http://nexeclient.xyz/bandini/access`} target="_blank"> link2</a>
+      <a href={`http://nexeclient.xyz/bandini/access/Unigine_Heaven-4.0.exe`} target="_blank"> link3</a>
     </div>)
 };
 
